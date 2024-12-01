c69e1f883946b056f3ac83eac76e0af7e76de268
bhall90 <ben.hall90@outlook.com>
2024-12-01 12:01:39 -0600

    Imported and sorted the list

--- 2024/Historian Hysteeria.py
+++ 2024/Historian Hysteeria.py
@@ -1,6 +1,19 @@
 # Import the values from the files and create two lists
-list1= open("testList1.txt").readlines()
-list2 = open("testList2.txt").readlines()
+from os import path
+pyFilePath = path.abspath(__file__)
+directoryPath = path.dirname(pyFilePath)
+list1FilePath = path.join(directoryPath, "testList1.txt")
+list2FilePath = path.join(directoryPath, "testList2.txt")
 
+with open(list1FilePath, 'r', encoding= "UTF-8") as file:
+    list1 = list(file)
+    list1 = [x.rstrip() for x in list1]
+
+
+# Sort the list
 print(list1)
-print(list2)
+list1 = list(map(int,list1))
+sortedList1 = sorted(list1)
+print(sortedList1)
+
+#find the lowest value while also removing it from the list
